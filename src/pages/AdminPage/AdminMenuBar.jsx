b9e4fa779c0d9df7cc17c513c37e2a6959b5d210
chatinunk97 <chatinun.hobby@gmail.com>
2023-10-12 14:08:41 +0700

    Admin page : add link to admin icon

--- src/pages/AdminPage/AdminMenuBar.jsx
+++ src/pages/AdminPage/AdminMenuBar.jsx
@@ -5,12 +5,12 @@ import SubMenuButton from "../../components/SubMenuButton";
 export default function AdminMenuBar() {
   return (
     <div className="bg-smoothgray flex r items-center">
-      <div className="bg-black flex justify-center items-center rounded-lg px-10 py-2">
+      <Link to={'/admin'} className="bg-black flex justify-center items-center rounded-lg px-10 py-2 cursor-pointer">
         <img src={adminIcon} alt="adminIcon" className="w-24" />
         <span className="text-white text-2xl font-semibold">
           User Management
         </span>
-      </div>
+      </Link>
       <div className="flex w-full justify-center gap-10 px-10">
         <SubMenuButton>
           {" "}
