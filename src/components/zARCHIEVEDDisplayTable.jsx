8671468421e715d504344983cdcf39f1c73e79c5
chatinunk97 <chatinun.hobby@gmail.com>
2023-12-25 21:17:21 +0700

    clear archived components

--- src/components/zARCHIEVEDDisplayTable.jsx
+++
@@ -1,9 +0,0 @@
-import "react-data-grid/lib/styles.css";
-import DataGrid from "react-data-grid";
-
-export default function TestTable({ data, columnFormat }) {
-  const columns = columnFormat;
-
-  const rows = data;
-  return <DataGrid columns={columns} rows={rows} className="rdg-light" />;
-}
